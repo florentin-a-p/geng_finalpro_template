dd456d8cdba1713e0b470df8b579016d9ddd4381
florentin.a.p <florentin.purnama@taralite.com>
2019-07-18 15:54:51 +0700

    coworking baru

--- coworking.py
+++ coworking.py
@@ -32,58 +32,42 @@ lebar_bata = ... # meter
 sisi_keramik = ... # meter
 
 # hitung jumlah keramik dari luas lantai
-def hitung_luas_lantai_total(...):
-    luas_lantai_total = org*luas_org # meter persegi
-    return luas_lantai_total
+# hitung luas lantai total
+luas_lantai_total = org*luas_org # meter persegi
 
-def hitung_jumlah_keramik(...,...,...):
-    luas_keramik_solo = ... # meter persegi
-    jumlah_keramik = ... # jumlah
-    return jumlah_keramik
+# hitung jumlah keramik
+luas_keramik_solo = ... # meter persegi
+jumlah_keramik = ... # jumlah
 
 # hitung jumlah bata dari luas tembok total (4 sisi), dikurangi pintu dan jendela
-def hitung_luas_pintu(...,...):
-    luas_pintu = ... # meter persegi
-    return luas_pintu
+# hitung luas pintu
+luas_pintu = ... # meter persegi
 
-def hitung_luas_jendela(...,...):
-    luas_jendela = ... # meter persegi
-    return luas_jendela
+# hitung luas jendela
+luas_jendela = ... # meter persegi
 
-def hitung_luas_tembok_asli(...):
-    panjang_sisi_tembok = ... # meter
-    luas_tembok_total = ... # meter persegi
-    luas_tembok_asli = luas_tembok_total - jumlah_pintu*luas_pintu - jumlah_jendela*luas_jendela # meter persegi
-    return luas_tembok_asli
+# hitung luas tembok asli
+panjang_sisi_tembok = ... # meter
+luas_tembok_total = ... # meter persegi
+luas_tembok_asli = luas_tembok_total - jumlah_pintu*luas_pintu - jumlah_jendela*luas_jendela # meter persegi
 
-def hitung_jumlah_bata(...,...,...):
-    luas_bata_solo = ... # meter persegi
-    jumlah_bata = ... # jumlah
-    return jumlah_bata
+# hitung jumlah bata
+luas_bata_solo = ... # meter persegi
+jumlah_bata = ... # jumlah
 
 # estimasi harga
 harga_bata_solo = ...
 harga_keramik_solo = ...
 profit = ...
 
-def hitung_harga_bangunan(...,...):
-    harga_bata_total = ...
-    harga_keramik_total = ...
-    return harga_bangunan
+# hitung harga bangunan
+harga_bata_total = ...
+harga_keramik_total = ...
+harga_bangunan = harga_bata_total + harga_keramik_total
 
-def hitung_harga_jual(...,...):
-    harga_jual = ... # berapa yang harus kamu jual agar tidak rugi? agar mendapatkan keuntungan?
-    return harga_jual
+# hitung harga jual
+harga_jual = ... # berapa yang harus kamu jual agar tidak rugi? agar mendapatkan keuntungan?
 
-# run all the functions
-luas_lantai_total = hitung_luas_lantai_total(...)
-jumlah_keramik = hitung_jumlah_keramik(...,...,...)
-luas_pintu = hitung_luas_pintu(...,...)
-luas_jendela = hitung_luas_jendela(...,...)
-luas_tembok_asli = hitung_luas_tembok_asli(...)
-jumlah_bata = hitung_jumlah_bata(...,...,...)
-harga_bangunan = hitung_harga_bangunantung_harga(...,...)
-harga_jual = hitung_harga_jual(...,...)
 
 print("===========================================================================================")
 print('Jumlah bata yang dibutuhkan: '+str(jumlah_bata))
